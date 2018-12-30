8fa1eba769a22ea1b2457309b285768451b49bf5
Pradeep Kandel <pradeep_kandel@hotmail.com>
2018-12-30 09:33:05 +0545

    Initial commit

---
+++ WebStoreNew/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebStoreNew.Areas.Admin.Models;
+using WebStoreNew.Models;
+
+namespace WebStoreNew.Areas.Admin.Controllers
+{
+    public class HomeController : Controller
+    {
+        private ApplicationDbContext dbusers = new ApplicationDbContext();
+        // GET: Admin/Home
+        public ActionResult Index()
+        {
+            var model = new DashboardViewModel();
+            model.Users = dbusers.Users.Count();
+            model.Categories = 12;
+            model.Products = 100;
+            model.Sales = 1200.00f;
+            return View(model);
+        }
+    }
+}
---
+++ WebStoreNew/Areas/Admin/Models/DashboardViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebStoreNew.Areas.Admin.Models
+{
+    public class DashboardViewModel
+    {
+        public int Users { get; set; }
+        public int Categories { get; set; }
+        public int Products { get; set; }
+        public float Sales { get; set; }
+    }
+}
